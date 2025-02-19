615f14b0fca57abad45edd9aef6947d34115af6d
GIORGIgok <g.goqadze1@gmail.com>
2025-02-19 22:59:48 +0400

    removed apolloprovider from layout.tsx which was accidentally added there by me

--- shopping-cart-challenge/src/app/layout.tsx
+++ shopping-cart-challenge/src/app/layout.tsx
@@ -1,18 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { ApolloProvider } from '@apollo/client';
-import apolloClient from '@/lib/graphql/apolloClient';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <html lang="en">
-        <body className="antialiased">{children}</body>
-      </html>
-    </ApolloProvider>
+    <html lang="en">
+      <body className="antialiased">{children}</body>
+    </html>
   );
 }
