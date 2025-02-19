4052e26ad135fc1fa4cbf53bdd38a6ca81fa15e3
GIORGIgok <g.goqadze1@gmail.com>
2025-02-20 01:45:57 +0400

    improved config of apollo, added some neccessary pages and styled main page

--- shopping-cart-challenge/src/app/page.tsx
+++ shopping-cart-challenge/src/app/page.tsx
@@ -1,5 +1,14 @@
 // import Image from 'next/image';
 
+import MainPageWelcome from '@/components/main-page-welcome/main-page-welcome';
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-4 bg-slate-800">
+      <h1 className="text-md bg-gradient-to-r from-blue-200 to-violet-500 bg-clip-text text-center font-semibold text-transparent sm:text-2xl md:text-4xl">
+        See why thousands choose our product.
+      </h1>
+      <MainPageWelcome />
+    </main>
+  );
 }
---
+++ shopping-cart-challenge/src/components/main-page-welcome/main-page-welcome.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const MainPageWelcome = () => {
+  return (
+    <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white transition duration-300 hover:bg-blue-700">
+      Sign up in seconds!
+    </button>
+  );
+};
+
+export default MainPageWelcome;
